b0a809f0c253dffe1a0799360e5640221c1524d9
AphilK <joaosilvanascimento.jops@gmail.com>
2022-09-25 15:04:22 -0300

    Comentários explicando o código adicionados

--- sequencias/sequencias_ciclicas.c
+++ sequencias/sequencias_ciclicas.c
@@ -10,17 +10,18 @@ int main() {
 
     printf("Vamos trabalhar com sequencias ciclicas!\n");
     printf("Insira a palavra/frase com a qual iremos trabalhar: ");
-    fgets(word, sizeof(word), stdin);  
+    fgets(word, sizeof(word), stdin);                                 //Recebe a palavra a ser usada
     printf("Agora diga-me a posicao desejada: ");
-    scanf("%d", &position);                                     
+    scanf("%d", &position);                                           //Recebe a posição da letra a qual irá encontrar                       
 
-    length = strlen(word) - 1;
-    if(position <= length) {
+    length = strlen(word) - 1;                                        //Definindo o tamanho da string
+
+    if(position <= length) {                                          //Explicação no readme
       position_real = position - 1;
     } else {
         position_real = (position % length)-1;
       }
     
-    printf("A letra na dada posicao e: %c", word[position_real]);  
+    printf("A letra na dada posicao e: %c", word[position_real]);     //Devolve ao usuário a letra desejada
     return 0;
 }
